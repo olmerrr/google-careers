ac0ab2e6767140cdd2fee594e88719c5322b1c5a
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-08 12:28:58 +0300

    add test for MainNav cmp more

--- src/test/unit/components/MainNav.test.js
+++ src/test/unit/components/MainNav.test.js
@@ -4,8 +4,16 @@ import MainNav from "@/components/MainNav.vue";
 describe("MainNav", () => {
   it("displays company name", () => {
     const wrapper = mount(MainNav);
+    // wecan use second arg in mount
+    // wrapper = mount(MainNav, {
+    //   data() {
+    //     return {
+    //       company: "Super Corp",
+    //     };
+    //   },
+    // });
     // console.log(wrapper.text());
     // console.log(wrapper.html());
-    expect(wrapper.text()).toMatch("Bobo Careers");
+    expect(wrapper.text()).toMatch("Super Corp");
   });
 });
