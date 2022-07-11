f5b6919ac9e1d8d143101f6751c17df7e09f321e
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-11 13:21:19 +0300

    add test for ActionButton cmp

---
+++ src/test/unit/components/ActionButton.test.js
@@ -0,0 +1,23 @@
+import { mount } from "@vue/test-utils";
+import ActionButton from "@/components/ActionButton.vue";
+
+describe("ActionButton", () => {
+  it("renders text", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "Some text",
+        type: "primary",
+      },
+    });
+    expect(wrapper.text()).toMatch("Some text");
+  });
+  it("applies one of several styles to button", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        type: "primary",
+      },
+    });
+    const button = wrapper.find("button");
+    expect(button.classes("primary")).toBe(true);
+  });
+});
