6a4c00d950fa6963ffc83f0142f6a56200e3e34e
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-11 19:26:24 +0300

    fix warning about FontAwesomeIcon in Jest

--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-16 bg-white border-b border-solid border-brand-gray-1">
     <div class="flex items-center h-full px-8">
-      <div>
+      <div v-if="onJobResultPage" data-test="job-count">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
         <span>
           <span class="text-brand-green-1">1234</span>
@@ -14,5 +14,10 @@
 <script>
 export default {
   name: "TheSubnav",
+  data() {
+    return {
+      onJobResultPage: true,
+    };
+  },
 };
 </script>
---
+++ src/test/unit/components/TheSubnav.test.js
@@ -0,0 +1,36 @@
+import { mount } from "@vue/test-utils";
+import TheSubnav from "@/components/TheSubnav.vue";
+
+describe("TheSubnav", () => {
+  describe("when user on job page", () => {
+    it("displays job count", () => {
+      const wrapper = mount(TheSubnav, {
+        global: {
+          stubs: {
+            FontAwesomeIcon: true,
+          },
+        },
+        data() {
+          return {
+            onJobResultPage: true,
+          };
+        },
+      });
+      const jobCount = wrapper.find("[data-test='job-count']");
+      expect(jobCount.exists()).toBe(true);
+    });
+  });
+  describe("when user not on job page", () => {
+    it("does not display job count", () => {
+      const wrapper = mount(TheSubnav, {
+        data() {
+          return {
+            onJobResultPage: false,
+          };
+        },
+      });
+      const jobCount = wrapper.find("[data-test='job-count']");
+      expect(jobCount.exists()).toBe(false);
+    });
+  });
+});
