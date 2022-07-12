e7433747fd6d05019e3769d522fc2475a2b83569
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-12 18:27:07 +0300

    add TheHerro cmp + TheHeadline cmp + work with lifecycle hook

---
+++ src/components/TheHeadline.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h1>Build for everyone</h1>
+    <h2>Find your next job at Bobo Corp.</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheHeadline",
+  data() {
+    return {
+      sample: "Hello Yaroslav",
+    };
+  },
+  created() {
+    console.log("Hey, i have been created!", this.sample);
+  },
+};
+</script>
+
+<style></style>
--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,9 +1,25 @@
 <template>
-  <main><section class=" ">Herro cmp123</section></main>
+  <main>
+    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1 border border-blue-500">1</div>
+        <div class="col-start-2 col-span-5 border border-blue-700">
+          <the-headline />
+        </div>
+        <div class="col-start-7 col-span-5 border border-blue-700">5</div>
+        <div class="col-start-12 col-span-1 border border-blue-500">1</div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
+import TheHeadline from "@/components/TheHeadline.vue";
+
 export default {
   name: "TheHero",
+  components: {
+    TheHeadline,
+  },
 };
 </script>
