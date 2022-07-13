690fa062f451dad39a4ac1625dfa4f6eee65175a
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-13 11:16:52 +0300

    add test nextElementInList

---
+++ src/test/unit/utils/nextElementInList.test.js
@@ -0,0 +1,18 @@
+import nextElementInList from "@/utils/nextElementInList";
+describe("nextElementInList", () => {
+  it("locates element in list and return next element in list", () => {
+    const list = ["A", "B", "C", "D", "E"];
+    const value = "C";
+    const result = nextElementInList(list, value);
+    expect(result).toBe("D");
+  });
+});
+
+describe("when element in the end of list", () => {
+  it("locates next element at start of the list", () => {
+    const list = ["A", "B", "C", "D", "E"];
+    const value = "E";
+    const result = nextElementInList(list, value);
+    expect(result).toBe("A");
+  });
+});
