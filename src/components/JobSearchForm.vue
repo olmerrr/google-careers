f5a957dd7679dfedec8f582d8bfbeab6840c3b68
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-14 15:06:18 +0300

    add new cmp JobSearchForm

---
+++ src/components/JobSearchForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <form>Form</form>
+</template>
+
+<script>
+export default {
+  name: "JobSearchForm",
+};
+</script>
+
+<style></style>
--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,13 +1,14 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
+    <section class="flex flex-col h-screen pt-10 pb-20">
       <div class="grid grid-cols-12">
-        <div class="col-start-1 col-span-1 border border-blue-500">1</div>
-        <div class="col-start-2 col-span-5 border border-blue-700">
+        <div class="col-start-1 col-span-1"></div>
+        <div class="col-start-2 col-span-5">
           <the-headline />
+          <job-search-form />
         </div>
-        <div class="col-start-7 col-span-5 border border-blue-700">5</div>
-        <div class="col-start-12 col-span-1 border border-blue-500">1</div>
+        <div class="col-start-7 col-span-5"></div>
+        <div class="col-start-12 col-span-1"></div>
       </div>
     </section>
   </main>
@@ -15,11 +16,13 @@
 
 <script>
 import TheHeadline from "@/components/TheHeadline.vue";
+import JobSearchForm from "@/components/JobSearchForm.vue";
 
 export default {
   name: "TheHero",
   components: {
     TheHeadline,
+    JobSearchForm,
   },
 };
 </script>
