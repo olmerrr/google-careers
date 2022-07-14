6e9561498961e4a43cbd45feb59f70f60616edf1
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-14 16:47:01 +0300

    add style for JobSearch cmp

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,10 +1,44 @@
 <template>
-  <form>Form</form>
+  <form
+    class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+    <div class="flex flex-nowrap flex-1 h-full text-base font-light">
+      <div class="relative flex items-center flex-1 h-full pr-3">
+        <label class="absolute left-0 -top-10">Role</label>
+        <input
+          type="text"
+          placeholder="Software engineer"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+
+      <span
+        class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2"
+        >in</span
+      >
+
+      <div class="relative flex items-center flex-1 h-full pl-3">
+        <label class="absolute left-0 -top-10">Where</label>
+        <input
+          type="text"
+          placeholder="Los Angeles"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+
+        <action-button text="Search" type="secondary" class="rounded-r-3xl" />
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
 export default {
   name: "JobSearchForm",
+  components: {
+    ActionButton,
+  },
 };
 </script>
 
--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="the-headline mb-16">
     <h1
       class="font-bold tracking-tighter text-8xl mb-14"
       data-test="action-phrase"
