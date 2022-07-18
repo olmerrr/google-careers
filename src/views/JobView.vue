85ddfae5e91cc56f26bebed3637b8b4483e50f22
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-18 13:31:33 +0300

    add view one page for Job

---
+++ src/views/JobView.vue
@@ -0,0 +1,19 @@
+<template>
+  <h4>
+    Job Viev for id:
+    <span>{{ currentJobId }}</span>
+  </h4>
+</template>
+
+<script>
+export default {
+  name: "JobView",
+  computed: {
+    currentJobId() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
+
+<style></style>
