f36f22311c26217e3f63e353ab79f9b337041ed0
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-19 13:59:14 +0300

    mapping data in child cmp JobListing

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -5,17 +5,23 @@
       class="block mx-auto bg-white border border-solid border-brand-gray-2 rounded hover:shadow-gray"
     >
       <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
-        <h2 class="mb-2 text-2xl">
-          Technical Program Manager, Perception, Augmented Reality
-        </h2>
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
 
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Bobo</span>
+            <span>{{ job.organization }}</span>
           </div>
 
           <div>
-            <span>San Francisco, CA, USA</span>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="inline-block mr-5"
+              >
+                {{ location }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -25,11 +31,11 @@
           <h3 class="mt-1 mb-2">Qualifications</h3>
           <div>
             <ul class="pl-8 list-disc">
-              <li>Bachelor's degree or equivalent practice experience.</li>
-              <li>5 years of experience in program management.</li>
-              <li>
-                Experience analyzng data through querying database (e.g. SQL),
-                using spreadsheet software, and creating statistical models
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
               </li>
             </ul>
           </div>
