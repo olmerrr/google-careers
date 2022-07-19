86277e764d001a2454eb96397643235c100be5a5
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-19 12:24:53 +0300

    add request for data jobs array

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -9,10 +9,22 @@
 </template>
 
 <script>
+import axios from "axios";
 import JobListing from "./JobListing.vue";
+
 export default {
   name: "JobListings",
   components: { JobListing },
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:3000/jobs").then((response) => {
+      this.jobs = response.data;
+    });
+  },
 };
 </script>
 
