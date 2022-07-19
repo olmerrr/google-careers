b746f798a660374a48196c7141a177dc9fccfd9b
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-19 16:48:32 +0300

    add more test for JobListing cmp

--- src/test/unit/components/JobResults/JobListing.test.js
+++ src/test/unit/components/JobResults/JobListing.test.js
@@ -5,6 +5,8 @@ describe("JobListing", () => {
   const createJobProps = (jobProps = {}) => ({
     title: "Angular Developer",
     organization: "Vue and Me",
+    location: ["Kiev"],
+    minimumQualifications: ["Development", "Testing"],
     ...jobProps,
   });
   const createConfig = (jobProps) => ({
@@ -29,5 +31,28 @@ describe("JobListing", () => {
     const jobProps = createJobProps({ organization: "Vue and Me" });
     const wrapper = mount(JobListing, createConfig(jobProps));
     expect(wrapper.text()).toMatch("Vue and Me");
+  });
+
+  it("renders job location", () => {
+    const jobProps = createJobProps({ locations: ["Orlando", "Jacksonville"] });
+    const wrapper = mount(JobListing, createConfig(jobProps));
+    expect(wrapper.text()).toMatch("Orlando");
+    expect(wrapper.text()).toMatch("Jacksonville");
+  });
+
+  it("renders job qualifications", () => {
+    const jobProps = createJobProps({
+      minimumQualifications: [
+        "Mesh granular deliverables, engineer enterprise convergence, and synergize B2C initiatives",
+        "Morph bricks-and-clicks relationships, whiteboard one-to-one experiences, and innovate distributed schemas",
+      ],
+    });
+    const wrapper = mount(JobListing, createConfig(jobProps));
+    expect(wrapper.text()).toMatch(
+      "Mesh granular deliverables, engineer enterprise convergence, and synergize B2C initiatives"
+    );
+    expect(wrapper.text()).toMatch(
+      "Morph bricks-and-clicks relationships, whiteboard one-to-one experiences, and innovate distributed schemas"
+    );
   });
 });
