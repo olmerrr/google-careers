3aef2205e89fb91399bd89678a5c93045cb540fd
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-20 11:05:56 +0300

    refactoring test for JobListings cmp

--- src/test/unit/components/JobResults/JobListings.test.js
+++ src/test/unit/components/JobResults/JobListings.test.js
@@ -5,15 +5,37 @@ jest.mock("axios");
 import JobListings from "@/components/JobResults/JobListings.vue";
 
 describe("JobListings", () => {
+  const $route = {
+    query: {
+      page: "5",
+    },
+  };
   it("fetches jobs list", async () => {
     axios.get.mockResolvedValue({ data: [] });
-    shallowMount(JobListings);
+    shallowMount(JobListings, {
+      global: {
+        mocks: {
+          $route,
+        },
+      },
+    });
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("creates a job list for a maxium 10 jobs", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
-    const wrapper = shallowMount(JobListings);
+    const $route = {
+      query: {
+        page: "1",
+      },
+    };
+    const wrapper = shallowMount(JobListings, {
+      global: {
+        mocks: {
+          $route,
+        },
+      },
+    });
     await flushPromises();
     const jobListings = wrapper.findAll("[data-test='job-listing']");
     expect(jobListings).toHaveLength(10);
