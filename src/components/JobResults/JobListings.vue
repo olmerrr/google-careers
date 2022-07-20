6391c7184bd585f79d9ddcba8cd1bc6b89eff4e9
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-20 13:08:08 +0300

    add next/prev btn + logic for routes

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -12,6 +12,21 @@
       <div class="flex flex-row flex-nowrap">
         <p class="text-sm flex-grow">Page: {{ currentPage }}</p>
       </div>
+
+      <div class="flex items-center justify-end">
+        <router-link
+          v-if="previousPage"
+          class="mx-3 text-sm font-semibold text-brand-blue-1"
+          :to="{ name: 'JobResults', query: { page: previousPage } }"
+          >Previous
+        </router-link>
+        <router-link
+          v-if="nextPage"
+          class="mx-3 text-sm font-semibold text-brand-blue-1"
+          :to="{ name: 'JobResults', query: { page: nextPage } }"
+          >Next</router-link
+        >
+      </div>
     </div>
   </main>
 </template>
@@ -32,6 +47,16 @@ export default {
     currentPage() {
       const pageString = this.$route.query.page || "1";
       return Number.parseInt(pageString);
+    },
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+      return previousPage >= firstPage ? previousPage : undefined;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const maxPage = this.jobs.length / 10;
+      return nextPage <= maxPage ? nextPage : undefined;
     },
     displayedJobs() {
       const pageNumber = this.currentPage;
