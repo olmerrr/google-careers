42c1890c93de4f5edaffe18c34418ff86dfb1a34
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-22 13:49:20 +0300

    add more test for actions

--- src/test/unit/store/index.test.js
+++ src/test/unit/store/index.test.js
@@ -1,4 +1,7 @@
-import { state, mutations } from "@/store";
+import { state, mutations, actions } from "@/store";
+import getJobs from "@/api/getJobs";
+
+jest.mock("@/api/getJobs");
 
 describe("state", () => {
   it("keeps track of whether user is logged in", () => {
@@ -26,6 +29,36 @@ describe("mutations", () => {
       };
       mutations.RECEIVE_JOBS(state, ["Job 1", "Job 2"]);
       expect(state).toEqual({ jobs: ["Job 1", "Job 2"] });
+    });
+  });
+});
+
+describe("actions", () => {
+  describe("FETCH_JOBS", () => {
+    beforeEach(() => {
+      getJobs.mockResolvedValue([
+        {
+          id: 1,
+          title: "Software Developer",
+        },
+      ]);
+    });
+    it("makes request to fetch jobs", async () => {
+      const context = { commit: jest.fn() };
+      await actions.FETCH_JOBS(context);
+      expect(getJobs).toHaveBeenCalled();
+    });
+
+    it("sends message to save received jobs in the store", async () => {
+      const commit = jest.fn();
+      const context = { commit };
+      await actions.FETCH_JOBS(context);
+      expect(commit).toHaveBeenCalledWith("RECEIVE_JOBS", [
+        {
+          id: 1,
+          title: "Software Developer",
+        },
+      ]);
     });
   });
 });
