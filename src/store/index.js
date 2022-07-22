721164a9308358cf1c71c058ac4d5eece232da85
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-22 13:22:17 +0300

    add action for FETCH_JOBS data

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore } from "vuex";
+import getJobs from "@/api/getJobs";
 
 export const LOGIN_USER = "LOGIN_USER";
 export const RECEIVE_JOBS = "RECEIVE_JOBS";
+export const FETCH_JOBS = "FETCH_JOBS";
 
 export const state = () => {
   return {
@@ -18,9 +20,18 @@ export const mutations = {
     state.jobs = jobs;
   },
 };
+
+export const actions = {
+  [FETCH_JOBS]: async (context) => {
+    const jobListings = await getJobs();
+    context.commit(RECEIVE_JOBS, jobListings);
+  },
+};
+
 const store = createStore({
   state,
   mutations,
+  actions,
   strict: process.env.NODE_ENV !== "production",
 });
 
