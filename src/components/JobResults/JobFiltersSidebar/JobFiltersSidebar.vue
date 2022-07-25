4aee8b41202b938da838774cbb86c601d5f3bbb6
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-25 19:14:18 +0300

    letle refactoring child cmp in JobFiltersSidebar

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -13,41 +13,20 @@
       <the-accordion header="Jobs types">
         <p>Placeholder</p>
       </the-accordion>
-      <the-accordion header="Organizations">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li class="w-1/2 h-8">
-                <input id="VueTube" type="checkbox" class="mr-3" />
-                <label for="VueTube">VueTube</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Between Vue and Me" type="checkbox" class="mr-3" />
-                <label for="Between Vue and Me">Between Vue</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-                <label for="Et Vue Brute">Et Vue Brute</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Vue and Half Men" type="checkbox" class="mr-3" />
-                <label for="Vue and Half Men">Vue and Half Men</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </the-accordion>
+      <job-filters-sidebar-organizations />
     </section>
   </div>
 </template>
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import TheAccordion from "@/components/Shared/TheAccordion.vue";
+import JobFiltersSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
 export default {
   name: "JobFiltersSidebar",
   components: {
     ActionButton,
     TheAccordion,
+    JobFiltersSidebarOrganizations,
   },
 };
 </script>
---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,38 @@
+<template>
+  <the-accordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li class="w-1/2 h-8">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="w-1/2 h-8">
+            <input id="Between Vue and Me" type="checkbox" class="mr-3" />
+            <label for="Between Vue and Me">Between Vue</label>
+          </li>
+          <li class="w-1/2 h-8">
+            <input id="Et Vue Brute" type="checkbox" class="mr-3" />
+            <label for="Et Vue Brute">Et Vue Brute</label>
+          </li>
+          <li class="w-1/2 h-8">
+            <input id="Vue and Half Men" type="checkbox" class="mr-3" />
+            <label for="Vue and Half Men">Vue and Half Men</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </the-accordion>
+</template>
+
+<script>
+import TheAccordion from "@/components/Shared/TheAccordion.vue";
+export default {
+  name: "JobFiltersSidebarOrganizations",
+  components: {
+    TheAccordion,
+  },
+};
+</script>
+
+<style></style>
