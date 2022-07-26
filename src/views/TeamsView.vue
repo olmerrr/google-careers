baf5a804062a3df08db76ccc036cd7558ed880c7
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-26 10:27:47 +0300

    add shortcut for v-slot

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,9 +1,9 @@
 <template>
   <header-container>
-    <template v-slot:title>
+    <template #title>
       <h1 class="w-full text-4xl font-normal">Teams</h1>
     </template>
-    <template v-slot:subtitle>
+    <template #subtitle>
       <h2 class="w-full my-4 text-base font-light">It's awesome</h2>
     </template>
   </header-container>
