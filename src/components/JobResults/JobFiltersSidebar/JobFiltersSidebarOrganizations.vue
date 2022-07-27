e41382649ed15d08ccc999d426d7c89846f437d9
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-07-27 13:04:28 +0300

    add getter on page + mapping data on cmp

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -3,21 +3,13 @@
     <div class="mt-5">
       <fieldset>
         <ul class="flex flex-row flex-wrap">
-          <li class="w-1/2 h-8">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube">VueTube</label>
-          </li>
-          <li class="w-1/2 h-8">
-            <input id="Between Vue and Me" type="checkbox" class="mr-3" />
-            <label for="Between Vue and Me">Between Vue</label>
-          </li>
-          <li class="w-1/2 h-8">
-            <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-            <label for="Et Vue Brute">Et Vue Brute</label>
-          </li>
-          <li class="w-1/2 h-8">
-            <input id="Vue and Half Men" type="checkbox" class="mr-3" />
-            <label for="Vue and Half Men">Vue and Half Men</label>
+          <li
+            v-for="organization in UNIQUE_ORGANIZATIONS"
+            :key="organization"
+            class="w-1/2 h-8"
+          >
+            <input id="organization" type="checkbox" class="mr-3" />
+            <label for="organization">{{ organization }}</label>
           </li>
         </ul>
       </fieldset>
@@ -31,6 +23,11 @@ export default {
   name: "JobFiltersSidebarOrganizations",
   components: {
     TheAccordion,
+  },
+  computed: {
+    UNIQUE_ORGANIZATIONS() {
+      return this.$store.getters.UNIQUE_ORGANIZATIONS;
+    },
   },
 };
 </script>
