1ec2d71137f91657e606ecc84e36afb34bb3a317
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-08-01 19:42:54 +0300

    update tests

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,6 @@
 import {
   UNIQUE_ORGANIZATIONS,
-  FILTERED_JOBS_BY_ORGANIZATIONS,
   UNIQUE_JOB_TYPES,
-  FILTERED_JOBS_BY_JOB_TYPES,
   FILTERED_JOBS,
   INCLUDE_JOB_BY_ORGANIZATION,
   INCLUDE_JOB_BY_JOB_TYPE,
@@ -14,27 +12,12 @@ const getters = {
     state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
     return uniqueOrganizations;
   },
-  [FILTERED_JOBS_BY_ORGANIZATIONS](state) {
-    if (state.selectedOrganizations.length === 0) {
-      return state.jobs;
-    }
-    return state.jobs.filter((job) =>
-      state.selectedOrganizations.includes(job.organization)
-    );
-  },
   [UNIQUE_JOB_TYPES](state) {
     const uniqueJobTypes = new Set();
     state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
     return uniqueJobTypes;
   },
-  [FILTERED_JOBS_BY_JOB_TYPES](state) {
-    if (state.selectedJobTypes.length === 0) {
-      return state.jobs;
-    }
-    return state.jobs.filter((job) =>
-      state.selectedJobTypes.includes(job.jobType)
-    );
-  },
+
   [INCLUDE_JOB_BY_ORGANIZATION]: (state) => (job) => {
     if (state.selectedOrganizations.length === 0) return true;
     return state.selectedOrganizations.includes(job.organization);
@@ -44,7 +27,6 @@ const getters = {
     return state.selectedJobTypes.includes(job.jobType);
   },
   [FILTERED_JOBS](state, getters) {
-    //
     return state.jobs
       .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
       .filter((job) => getters.INCLUDE_JOB_BY_JOB_TYPE(job));
--- src/test/unit/store/getters.test.js
+++ src/test/unit/store/getters.test.js
@@ -14,41 +14,7 @@ describe("getters", () => {
       expect(result).toEqual(new Set(["Google", "Amazon"]));
     });
   });
-  describe("FILTERED_JOBS_BY_ORGANIZATIONS", () => {
-    it("identifies jobs that are associated with the given organizations", () => {
-      const state = {
-        jobs: [
-          { organization: "Google" },
-          { organization: "Amazon" },
-          { organization: "Microsoft" },
-        ],
-        selectedOrganizations: ["Google", "Microsoft"],
-      };
-      const filteredJobs = getters.FILTERED_JOBS_BY_ORGANIZATIONS(state);
-      expect(filteredJobs).toEqual([
-        { organization: "Google" },
-        { organization: "Microsoft" },
-      ]);
-    });
-    describe("when the user has not selected organizations", () => {
-      it("return all jobs", () => {
-        const state = {
-          jobs: [
-            { organization: "Google" },
-            { organization: "Amazon" },
-            { organization: "Microsoft" },
-          ],
-          selectedOrganizations: [],
-        };
-        const filteredJobs = getters.FILTERED_JOBS_BY_ORGANIZATIONS(state);
-        expect(filteredJobs).toEqual([
-          { organization: "Google" },
-          { organization: "Amazon" },
-          { organization: "Microsoft" },
-        ]);
-      });
-    });
-  });
+
   describe("UNIQUE_JOB_TYPES", () => {
     it("finds unique job types from list of jobs", () => {
       const state = {
@@ -63,41 +29,6 @@ describe("getters", () => {
     });
   });
 
-  describe("FILTERED_JOBS_BY_JOB_TYPES", () => {
-    it("identifies jobs that are associated with the given job types", () => {
-      const state = {
-        jobs: [
-          { jobType: "Full-time" },
-          { jobType: "Temporary" },
-          { jobType: "Part-time" },
-        ],
-        selectedJobTypes: ["Full-time", "Part-time"],
-      };
-      const filteredJobs = getters.FILTERED_JOBS_BY_JOB_TYPES(state);
-      expect(filteredJobs).toEqual([
-        { jobType: "Full-time" },
-        { jobType: "Part-time" },
-      ]);
-    });
-    describe("when the user has not selected any job types", () => {
-      it("return all jobs", () => {
-        const state = {
-          jobs: [
-            { jobType: "Full-time" },
-            { jobType: "Temporary" },
-            { jobType: "Part-time" },
-          ],
-          selectedJobTypes: [],
-        };
-        const filteredJobs = getters.FILTERED_JOBS_BY_JOB_TYPES(state);
-        expect(filteredJobs).toEqual([
-          { jobType: "Full-time" },
-          { jobType: "Temporary" },
-          { jobType: "Part-time" },
-        ]);
-      });
-    });
-  });
   describe("INCLUDE_JOB_BY_ORGANIZATION", () => {
     describe("when the user has not selected any organizations", () => {
       it("includes job", () => {
@@ -120,6 +51,7 @@ describe("getters", () => {
       });
     });
   });
+
   describe("INCLUDE_JOB_BY_JOB_TYPE", () => {
     describe("when the user has not selected any job types", () => {
       it("includes job", () => {
