94da4075e7e71e69caad27004d87dc839086915f
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-08-03 13:02:47 +0300

    update tests for TheSubnav cmp

--- src/test/unit/components/Navigation/TheSubnav.test.js
+++ src/test/unit/components/Navigation/TheSubnav.test.js
@@ -1,16 +1,15 @@
 import { mount } from "@vue/test-utils";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+
+jest.mock("vuex");
+jest.mock("vue-router");
 
 import TheSubnav from "@/components/Navigation/TheSubnav";
 
 describe("TheSubnav", () => {
-  const createConfig = (routeName, $store = {}) => ({
+  const createConfig = () => ({
     global: {
-      mocks: {
-        $route: {
-          name: routeName,
-        },
-        $store,
-      },
       stubs: {
         FontAwesomeIcon: true,
       },
@@ -19,13 +18,17 @@ describe("TheSubnav", () => {
 
   describe("when user is on job page", () => {
     it("displays job count", () => {
-      const routeName = "JobResults";
-      const $store = {
+      useRoute.mockReturnValue({
+        name: "JobResults",
+      });
+
+      useStore.mockReturnValue({
         getters: {
           FILTERED_JOBS: [{ id: 1 }, { id: 2 }],
         },
-      };
-      const wrapper = mount(TheSubnav, createConfig(routeName, $store));
+      });
+
+      const wrapper = mount(TheSubnav, createConfig());
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.text()).toMatch("2 jobs matched");
     });
@@ -33,8 +36,15 @@ describe("TheSubnav", () => {
 
   describe("when user is not on jobs page", () => {
     it("does NOT display job count", () => {
-      const routeName = "Home";
-      const wrapper = mount(TheSubnav, createConfig(routeName));
+      useRoute.mockReturnValue({
+        name: "Home",
+      });
+      useStore.mockReturnValue({
+        getters: {
+          FILTERED_JOBS: [],
+        },
+      });
+      const wrapper = mount(TheSubnav, createConfig());
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(false);
     });
