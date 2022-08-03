741d54ea843c5af7ffe1625f49cea2eea1e155d3
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-08-03 18:42:05 +0300

    add JobFiltersSidebarJobTypes refactoring to composition api

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue
@@ -4,7 +4,7 @@
       <fieldset>
         <ul class="flex flex-row flex-wrap">
           <li
-            v-for="jobType in UNIQUE_JOB_TYPES"
+            v-for="jobType in uniqueJobTypes"
             :key="jobType"
             class="w-1/2 h-8"
           >
@@ -26,8 +26,12 @@
 </template>
 
 <script>
-import { mapGetters, mapMutations } from "vuex";
-import { UNIQUE_JOB_TYPES, ADD_SELECTED_JOB_TYPES } from "@/store/constants";
+import { ref } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+
+import { useUniqueJobTypes } from "@/store/composables";
+import { ADD_SELECTED_JOB_TYPES } from "@/store/constants";
 
 import TheAccordion from "@/components/Shared/TheAccordion.vue";
 
@@ -36,20 +40,19 @@ export default {
   components: {
     TheAccordion,
   },
-  data() {
-    return {
-      selectedJobTypes: [],
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+
+    const selectedJobTypes = ref([]);
+    const uniqueJobTypes = useUniqueJobTypes();
+
+    const selectJobType = () => {
+      store.commit(ADD_SELECTED_JOB_TYPES, selectedJobTypes.value);
+      router.push({ name: "JobResults" });
     };
-  },
-  computed: {
-    ...mapGetters([UNIQUE_JOB_TYPES]),
-  },
-  methods: {
-    ...mapMutations([ADD_SELECTED_JOB_TYPES]),
-    selectJobType() {
-      this.ADD_SELECTED_JOB_TYPES(this.selectedJobTypes);
-      this.$router.push({ name: "JobResults" });
-    },
+
+    return { selectedJobTypes, uniqueJobTypes, selectJobType };
   },
 };
 </script>
