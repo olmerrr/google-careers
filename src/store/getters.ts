1831cf3dcfa7e1f051ad2ce819b0ba3a02798418
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-08-05 19:03:10 +0300

    add refactoring for getters

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,3 +1,6 @@
+import { GlobalState } from "@/store/types";
+import { Job } from "@/api/types";
+
 import {
   UNIQUE_ORGANIZATIONS,
   UNIQUE_JOB_TYPES,
@@ -5,14 +8,6 @@ import {
   INCLUDE_JOB_BY_ORGANIZATION,
   INCLUDE_JOB_BY_JOB_TYPE,
 } from "@/store/constants";
-
-import { GlobalState } from "@/store/types";
-import { Job } from "@/api/types";
-
-interface IncludeJobGetters {
-  INCLUDE_JOB_BY_ORGANIZATION: (job: Job) => boolean;
-  INCLUDE_JOB_BY_JOB_TYPE: (job: Job) => boolean;
-}
 
 const getters = {
   [UNIQUE_ORGANIZATIONS](state: GlobalState) {
@@ -25,6 +20,7 @@ const getters = {
     state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
     return uniqueJobTypes;
   },
+
   [INCLUDE_JOB_BY_ORGANIZATION]: (state: GlobalState) => (job: Job) => {
     if (state.selectedOrganizations.length === 0) return true;
     return state.selectedOrganizations.includes(job.organization);
@@ -33,7 +29,7 @@ const getters = {
     if (state.selectedJobTypes.length === 0) return true;
     return state.selectedJobTypes.includes(job.jobType);
   },
-  [FILTERED_JOBS](state: GlobalState, getters: IncludeJobGetters) {
+  [FILTERED_JOBS](state: GlobalState, getters) {
     return state.jobs
       .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
       .filter((job) => getters.INCLUDE_JOB_BY_JOB_TYPE(job));
