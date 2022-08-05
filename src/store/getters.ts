9f3f13504a0dc41e7ce6cf5c61757f072f4f6cd3
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-08-05 19:12:51 +0300

    fix bug in getters

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,6 +1,11 @@
 import { GlobalState } from "@/store/types";
 import { Job } from "@/api/types";
 
+interface IncludeJobGetters {
+  INCLUDE_JOB_BY_ORGANIZATION: (job: Job) => boolean;
+  INCLUDE_JOB_BY_JOB_TYPE: (job: Job) => boolean;
+}
+
 import {
   UNIQUE_ORGANIZATIONS,
   UNIQUE_JOB_TYPES,
@@ -29,7 +34,7 @@ const getters = {
     if (state.selectedJobTypes.length === 0) return true;
     return state.selectedJobTypes.includes(job.jobType);
   },
-  [FILTERED_JOBS](state: GlobalState, getters) {
+  [FILTERED_JOBS](state: GlobalState, getters: IncludeJobGetters) {
     return state.jobs
       .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
       .filter((job) => getters.INCLUDE_JOB_BY_JOB_TYPE(job));
