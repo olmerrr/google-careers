715ade62734d9bf6e38b8ba36daa138db93af581
Yaroslav Dombrovskiy <dombrovskiy91@gmail.com>
2022-08-07 16:44:28 +0300

    add actions + ts

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,8 +1,14 @@
+import { Commit } from "vuex";
+
 import { FETCH_JOBS, RECEIVE_JOBS } from "@/store/constants";
 import getJobs from "@/api/getJobs";
 
+interface Context {
+  commit: Commit;
+}
+
 const actions = {
-  [FETCH_JOBS]: async (context) => {
+  [FETCH_JOBS]: async (context: Context) => {
     const jobListings = await getJobs();
     context.commit(RECEIVE_JOBS, jobListings);
   },
